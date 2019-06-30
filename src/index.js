b8fa97901f846524261b810b61f6821f562b3f66
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-06-30 21:13:28 +0900

    Update index.js Create store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import reducers from './modules';
+
+// create store
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />,
+    </Provider>,
     document.getElementById('root')
 );
